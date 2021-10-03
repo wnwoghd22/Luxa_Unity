bc5f7f7b105704f23c9c647dc940b07929fadb16
wnwoghd22 <53481297+wnwoghd22@users.noreply.github.com>
2021-10-03 18:53:40 +0900

    making file system

--- Assets/Scripts/FileManager.cs
+++ Assets/Scripts/FileManager.cs
@@ -19,4 +19,19 @@ public class FileManager {
 
         return b;
     }
+    public Board ReadStageFileForAndroid(int n) {
+        //string filePath = Path.Combine(Application.streamingAssetsPath, "Stages/" + n + ".txt");
+        //StreamReader file = new StreamReader(filePath);
+
+        //string boardSize = file.ReadLine();
+        //string[] size = boardSize.Split(' ');
+
+        //Board b = new Board(Convert.ToInt32(size[0]), Convert.ToInt32(size[1]));
+
+        //Debug.Log(size[0] + size[1]);
+
+        //return b;
+
+        return null;
+    } 
 }
